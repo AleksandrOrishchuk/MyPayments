cd7e6b3a2dd799c2339cb2c56518e337da9b18fb
AleksandrOrishchuk <alexbiohate@gmail.com>
2021-02-20 18:34:32 +0300

    Added view binding to login fragment.

--- app/src/main/java/com/ssho/aeontest/ui/LoginFragment.kt
+++ app/src/main/java/com/ssho/aeontest/ui/LoginFragment.kt
@@ -1,6 +1,23 @@
 package com.ssho.aeontest.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ssho.aeontest.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var loginBinding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        loginBinding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        return loginBinding.root
+    }
 }
